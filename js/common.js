947450141776f5060fb3b3151f6d4d6e9b77c682
binhnx285 <binhnx285@gmail.com>
2020-03-20 00:12:55 +0700

    hehe

---
+++ js/common.js
@@ -0,0 +1,14 @@
+﻿GoogleLogin = function (link) {
+    var w = 500;
+    var h = 600;
+    const dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop = window.screenTop !== undefined ? window.screenTop : window.screenY;
+
+    const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+    const left = (width - w) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - h) / 2 / systemZoom + dualScreenTop;
+    window.open(link, "_blank", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no,top=" + top + ",left=" + left + ",width=" + w + ",height=" + h);
+};
